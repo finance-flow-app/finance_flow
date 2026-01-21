7c0fd5c530475b41d836be8743c754a4dcb81608
Artem Chikolaev <SecretMikasa@mail.ru>
2026-01-21 17:12:07 +0300

    include flutter_gen, build_runner and flutter_svg for assets and pictures

--- lib/src/features/home/presentation/pages/home_page.dart
+++ lib/src/features/home/presentation/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:finance_flow/core/generated/assets/assets.gen.dart';
 import 'package:finance_flow/core/generated/localization/locale_keys.g.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +15,10 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(LocaleKeys.hello_world.tr()),
+            const SizedBox(height: 20),
+            Assets.images.testSvgIcon.svg(width: 220, height: 220),
+            const SizedBox(height: 20),
+            Assets.images.testSvgIcon2.svg(width: 220, height: 220),
           ],
         ),
       ),
